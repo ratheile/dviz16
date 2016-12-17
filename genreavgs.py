237db5ed126af49b90980d017d724b32d89e4086
Raffael Theiler <raffael.theiler@uzh.ch>
2016-12-17 18:56:33 +0100

    task4

---
+++ genreavgs.py
@@ -0,0 +1,100 @@
+import functools as ft
+import multiprocessing as mp
+import numpy as np
+import json
+
+import plotly.graph_objs as go
+import plotly.offline as offline
+from plotly import tools
+
+def containsdigit(s):
+    if any(char.isdigit() for char in s) == True:
+        return True
+    else:
+        return False
+
+def extractnr(x):
+    return int(''.join(ele for ele in x if ele.isdigit()))
+
+def extract_movie_and_year(tokens, start):   
+    moviename = ""
+    year = None
+    appendnext = True
+    ismovie = True
+    ismovienow = False
+    for t in range(start, len(tokens)-1):
+        ismovienow = ismovie
+        if tokens[t+1].startswith('(1') == False and tokens[t+1].startswith('(2') == False and ismovienow == True:
+            appendnext = True
+        elif tokens[t+1].startswith('(1') == True or tokens[t+1].startswith('(2') == True :
+            ismovie = False
+            year = extractnr(tokens[t+1])
+                
+        if appendnext == True and ismovienow == True:
+            moviename = moviename + str(" " + tokens[t])
+            appendnext == False
+    return [moviename, year]
+
+
+#returns [name, year, rating, votes, length]
+def processRatingsLine(line): 
+    tokens = line.split()
+    rating = tokens[2]
+    nr_votes = tokens[1]
+    mov_year = extract_movie_and_year(tokens,3)
+    mov_year.append(float(rating))
+    mov_year.append(int(nr_votes))
+    mov_year.append(-1)
+    return [(mov_year[0], mov_year[1]), rating]
+    
+    
+def processGenres(line):
+    tokens = line.split()
+    genre = tokens[-1]
+    mov_year = extract_movie_and_year(tokens, 0)
+    mov_year.append(genre)
+    return [(mov_year[0], mov_year[1]), genre, -0.1]
+
+filename = "ratings.txt"
+with open(filename, "r", encoding="latin-1") as f:
+    ratings = f.readlines()[297:695711]
+print("read file: ratings.txt")
+
+filename = "genres.txt"
+with open(filename, "r", encoding="latin-1") as f:
+    genres = f.readlines()[385:2397982]
+print("read file: running-times.txt")
+
+pool = mp.Pool()
+ratings_list = pool.map(processRatingsLine, ratings)
+print("rating list created: ", len(ratings_list))
+
+pool = mp.Pool()
+genres_list = pool.map(processGenres, genres)
+print("genres list created: ", len(genres_list))
+
+rating_d = dict(ratings_list)
+
+for genre_entry in genres_list:
+    if genre_entry[0] in rating_d:
+        genre_entry[2] = float(rating_d[genre_entry[0]]) 
+
+
+data_with_rating= list(filter(lambda x:x[2] and x[2] > 0, genres_list))
+
+np_genres = np.array(data_with_rating)
+
+uni_gen = np.unique(np_genres[:,[1]])
+
+genre_avgs = {}
+for gen in uni_gen:    
+    subset = list(filter(lambda x: x[1] == gen, np_genres))
+    count = 0
+    scores = 0.0
+    for movie in subset:
+        count = count+1
+        scores = scores + movie[2]
+    genre_avgs[gen] = scores / count
+
+with open('static/genreavgs.json', 'w') as outfile:
+    json.dump(genre_avgs, outfile)
--- static/app.js
+++ static/app.js
@@ -17,9 +17,67 @@ $(document).ready(function(){
 
 	$("#button4").click(function(e){
 		e.preventDefault();
-		load('/test',{}, '#plot4');
+		$.getJSON("static/genreavgs.json", function(json) {
+
+   			var keys = [];
+   			var values = []
+			for (var key in json) {
+			  if (json.hasOwnProperty(key)) {
+			    keys.push(key);
+				values.push(json[key]);
+			  }
+			}
+
+			var min = Math.min.apply(null, values); 
+			var max = Math.max.apply(null, values); 
+			var count = keys.length;
+			var pi = Math.PI;
+
+			var plot_size = 600;
+			var radius = 100
+
+			var min_font_px = 15;
+			var max_font_px = 60;
+
+			var svg = d3.select("#plot4").append("svg")
+			                             .attr("width", plot_size)
+			                             .attr("height", plot_size);
+
+
+			for(var i = 0; i < count; i++){
+
+				var delta = max_font_px - min_font_px;
+				var scale = (json[keys[i]] - min) / (max - min) ;
+				var font_size = min_font_px + delta * scale; 
+
+				 svg.append("text")
+				.text(keys[i])
+				.style("font-size", font_size + "px"   )
+				.style('fill', getRandomColor())
+				.attr("width", 10)
+				.attr("height", 10)
+				.attr("x", plot_size/2 + radius)
+				.attr("y", plot_size/2)
+				.attr("transform","rotate(" + (360/count) * i + "," 
+					+ plot_size/2 + ","
+					+ plot_size/2 + ")");
+
+			}
+
+	
+
+		});
 	});
 
+
+	function getRandomColor() {
+	    var letters = '0123456789ABCDEF';
+	    var color = '#';
+	    for (var i = 0; i < 6; i++ ) {
+	        color += letters[Math.floor(Math.random() * 16)];
+	    }
+	    return color;
+	}
 
 	function load(url, data, parent){
 		$.ajax({type: "GET",
